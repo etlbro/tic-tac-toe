9a1060940575dfc3cd7b17377c656ec2b4d63d22
etlbro <eazbrody@gmail.com>
2025-09-28 17:00:24 +0300

    implamented and tested player logic

--- game_logic.py
+++ game_logic.py
@@ -1,5 +1,8 @@
 
 import board as b 
+import player
+from  itertools import cycle
+
 
 class Game_logic:
     '''
@@ -43,7 +46,36 @@ class Game_logic:
         '''
         self.board.reset_board()
 
+    #test function. in actuallity lpayer will have a few options for players (pvp, pvc) so we'll need the factory. for now still with this
+    def init_player(self):
+        player_1 = player.HumanPlayer('x') #make instance of human player with selected symbol
+        player_2 = player.HardPlayer('o')
 
 
 
+    def run_game(self,player_1_data, player_2_data):
+        '''
+        main function to run game, handle player turns and checkfor game endings
+        '''
+        try:
+            player_1 = player.PlayerFactory.creat_player(player_1_data) #make instance of human player with selected symbol
+            player_2 = player.PlayerFactory.creat_player(player_2_data) 
+        except Exception as e:
+            print("invalid player type or symbol")
+            return
+        players = [player_1, player_2] 
+        turns = cycle(players)
+            #player.HardPlayer("x"), player.HardPlayer("o")
+        for current_player in turns:
+            while True: # makes sure move is valid, keep trying until set (= make move return true)
+                move = current_player.get_move(self.board) 
+                if(self.make_move(current_player.get_symbol(),move[0], move[1])):
+                    break
 
+            self.board.print_board()                   
+            if self.game_ended():
+                print("game over")
+                break
+            if self.check_winner(current_player.get_symbol(), move[0],move[1]):
+                print(f"{current_player.get_symbol()} wins! well played!")
+                break
--- test_game_logic.py
+++ test_game_logic.py
@@ -1,7 +1,7 @@
 
 import unittest
 import game_logic as GL
-
+import player
 gl = GL.Game_logic(3)
 
 class TestPiecePlacment(unittest.TestCase):
@@ -73,8 +73,53 @@ class TestGameEnding(unittest.TestCase):
         gl.make_move('x', 2,1)   
         self.assertTrue(gl.check_winner('x', 2,1)) 
 
+    #def test_player_input(Self):
+    #    print()
+    #    gl.start_new_game()
+    #    player_1 = player.HumanPlayer('x')
+    #    move = player_1.get_move(gl.board) #get input
+    #    gl.make_move(player_1.get_symbol(), move[0], move[1])
+    #    gl.board.print_board()
 
 
+    #def test_rand_comp_player(self):
+    #    print()
+    #    gl.start_new_game()
+    #    easy_player = player.EasyComPlayer('x')
+    #    move = easy_player.get_move(gl.board) #get input
+    #    gl.make_move(easy_player.get_symbol(), move[0], move[1])
+    #    gl.board.print_board()
+
+
+
+    def test_hard_comp_player(self):
+        gl.start_new_game()
+        #check that best move is made
+        gl.make_move('x', 0,0) # x   o
+        gl.make_move('x', 1,0) # x   o
+        gl.make_move('o', 0,2) #
+        gl.make_move('o', 1,2) # check tht it know to end the game and not block
+        
+        hard_player = player.HardPlayer('x')
+        move = hard_player.get_move(gl.board)
+        assert move == (2 , 0) # best move
+
+
+    def test_full_game_easy(self):
+        '''
+        honestly not sure this will look
+        '''
+        gl.start_new_game()
+        gl.run_game(['easy', 'x'], ['easy', 'o'])
+        gl.board.print_board()
+
+    def test_full_game_hard(self):
+        '''
+        tic-tac-toe is a sloved game. should be a tie
+        '''
+        gl.start_new_game()
+        gl.run_game(['hard', 'x'], ['hard', 'o'])
+        gl.board.print_board()
 
 
 if __name__ == "__main__":
